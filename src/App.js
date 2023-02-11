d61c0d6a5cc113bd26af94797ba5280767aaea16
letsjo <hyunoh.jo@gmail.com>
2023-02-11 23:19:48 +0900

    feat(*): pages route init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const Login = lazy(() => import('../pages/Login'));
+const Join = lazy(() => import('../pages/Join'));
+const Todo = lazy(() => import('../pages/Todo'));
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Suspense fallback={ <div>Loading...</div> }>
+        <Routes>
+          <Route path='/' component={ Login } />
+          <Route path='/signin' component={ Login } />
+          <Route path='/signup' component={ Join } />
+          <Route path='/todo' component={ Todo } />
+        </Routes>
+      </Suspense>
+    </Router>
   );
 }
 
