ac7a833e54662f803d0885b2d30c1c02eb965559
letsjo <hyunoh.jo@gmail.com>
2023-02-14 20:15:11 +0900

    feat(CardDisplay): deleteTodoApi 연결

--- src/components/layout/todo/CardDisplay/index.jsx
+++ src/components/layout/todo/CardDisplay/index.jsx
@@ -1,13 +1,31 @@
 import { Styled } from './style';
 import SmallButton from 'components/layout/public/SmallButton';
+import { deleteTodoApi } from 'apis/todo';
 
-const CardDisplay = ({ modify }) => {
+const CardDisplay = ({ todoData, modify, setModify }) => {
+  const handleModify = () => {
+    setModify(true);
+  };
+
+  const handleDelete = ({ e }) => {
+    e.preventDefault();
+    deleteTodoApi({ id: todoData.id })
+      .then((res) => {
+        console.log(res);
+        alert('할 일이 삭제되었습니다.');
+      })
+      .catch((error) => {
+        console.log(error);
+        alert('할 일 삭제에 실패했습니다.');
+      });
+  };
+
   return (
     <Styled.DisplayWrap style={{ display: modify ? 'none' : 'flex' }}>
-      <Styled.CheckBox type='checkbox' />
-      <Styled.TodoTitle>할 일 적기</Styled.TodoTitle>
-      <SmallButton ButtonData={{ text: '수정' }} />
-      <SmallButton ButtonData={{ text: '삭제' }} />
+      <Styled.CheckBox type='checkbox' efaultChecked={todoData.isCompleted} />
+      <Styled.TodoTitle>{todoData.todo}</Styled.TodoTitle>
+      <SmallButton ButtonData={{ text: '수정', handleClick: handleModify }} />
+      <SmallButton ButtonData={{ text: '삭제', handleClick: handleDelete }} />
     </Styled.DisplayWrap>
   );
 };
