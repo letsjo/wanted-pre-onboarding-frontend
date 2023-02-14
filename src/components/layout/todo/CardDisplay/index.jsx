6946b4ea51adc875122a7d7f546ad7937968bb13
letsjo <hyunoh.jo@gmail.com>
2023-02-14 22:12:10 +0900

    feat(CardDisplay): isCompleted 기능 추가

--- src/components/layout/todo/CardDisplay/index.jsx
+++ src/components/layout/todo/CardDisplay/index.jsx
@@ -1,29 +1,55 @@
 import { Styled } from './style';
 import SmallButton from 'components/layout/public/SmallButton';
-import { deleteTodoApi } from 'apis/todo';
+import { deleteTodoApi, updateTodoApi } from 'apis/todo';
+import { useState } from 'react';
 
 const CardDisplay = ({ todoData, modify, setModify }) => {
+  const [todo, setTodo] = useState({ ...todoData });
+
   const handleModify = () => {
     setModify(true);
   };
 
-  const handleDelete = ({ e }) => {
-    e.preventDefault();
+  const handleIsCompleted = (e) => {
+    updateTodoApi({
+      id: todoData.id,
+      todo: todoData.todo,
+      isCompleted: e.target.checked,
+    })
+      .then(() => {
+        setTodo({ ...todo, isCompleted: e.target.checked });
+        e.target.checked
+          ? alert(`[${todoData.todo}] 를 완료했습니다.`)
+          : alert(`[${todoData.todo}] 를 시작해주세요!`);
+      })
+      .catch((error) => {
+        e.target.checked = todo.isCompleted;
+        alert(
+          `완료 처리를 실패하였습니다. \nERROR : ${error.response.data.message}`,
+        );
+      });
+  };
+
+  const handleDelete = () => {
     deleteTodoApi({ id: todoData.id })
-      .then((res) => {
-        console.log(res);
+      .then(() => {
         alert('할 일이 삭제되었습니다.');
       })
       .catch((error) => {
-        console.log(error);
-        alert('할 일 삭제에 실패했습니다.');
+        alert(
+          `삭제 처리를 실패하였습니다.\nERROR : ${error.response.data.message}`,
+        );
       });
   };
 
   return (
     <Styled.DisplayWrap style={{ display: modify ? 'none' : 'flex' }}>
-      <Styled.CheckBox type='checkbox' efaultChecked={todoData.isCompleted} />
-      <Styled.TodoTitle>{todoData.todo}</Styled.TodoTitle>
+      <Styled.CheckBox
+        type='checkbox'
+        defaultChecked={todo.isCompleted}
+        onChange={(e) => handleIsCompleted(e)}
+      />
+      <Styled.TodoTitle>{todo.todo}</Styled.TodoTitle>
       <SmallButton ButtonData={{ text: '수정', handleClick: handleModify }} />
       <SmallButton ButtonData={{ text: '삭제', handleClick: handleDelete }} />
     </Styled.DisplayWrap>
