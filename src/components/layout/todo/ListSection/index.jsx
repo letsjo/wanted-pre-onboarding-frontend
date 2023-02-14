078fe4ec345b5df77809c01d93b37927156d86d3
letsjo <hyunoh.jo@gmail.com>
2023-02-14 19:25:58 +0900

    refactor(ListSection): getTodoApi 적용

--- src/components/layout/todo/ListSection/index.jsx
+++ src/components/layout/todo/ListSection/index.jsx
@@ -1,15 +1,30 @@
+import { useEffect, useState } from 'react';
 import Card from '../Card';
 import { Styled } from './style';
+import { getTodoApi } from 'apis/todo';
 
 const ListSection = () => {
+  const [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+    const getTodoList = () => {
+      getTodoApi()
+        .then((res) => {
+          setTodoList(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    getTodoList();
+  }, []);
+
   return (
     <Styled.Container>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {todoList &&
+        todoList.map((todoData) => (
+          <Card key={todoData.id} todoData={todoData} />
+        ))}
     </Styled.Container>
   );
 };
