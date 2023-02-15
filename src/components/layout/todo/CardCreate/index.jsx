dfb97d4999cb00ee896c8a3bece2c063bbb48f6c
letsjo <hyunoh.jo@gmail.com>
2023-02-15 21:36:33 +0900

    rename(CardCreate): component 구분을 위해서 변경

--- src/components/layout/todo/CardCreate/index.jsx
+++ src/components/layout/todo/CardCreate/index.jsx
@@ -1,10 +1,14 @@
 import Input from 'components/layout/public/Input';
 import { Styled } from './style';
 import SmallButton from 'components/layout/public/SmallButton';
-import { useState } from 'react';
+import { useContext, useRef, useState } from 'react';
 import { createTodoApi } from 'apis/todo';
+import { dispatchContext } from 'context/TodoProvider';
 
-const CreateCard = () => {
+const CardCreate = () => {
+  const dispatch = useContext(dispatchContext);
+  const inputRef = useRef();
+
   const [todo, setTodo] = useState('');
 
   const onChangeTodo = (e) => {
@@ -12,13 +16,13 @@ const CreateCard = () => {
   };
 
   const handleCreate = ({ e }) => {
-    console.log(e);
     e.preventDefault();
     if (todo.trim().length <= 0) return alert('공백은 추가 할 수 없습니다.');
     if (typeof todo !== 'string') return alert('문자형만 추가할 수 있습니다.');
     createTodoApi({ todo })
-      .then(() => {
-        setTodo('');
+      .then((res) => {
+        inputRef.current.value = '';
+        dispatch({ type: 'CREATE', payload: res.data });
         alert('할 일이 추가되었습니다.');
       })
       .catch((error) => {
@@ -37,6 +41,7 @@ const CreateCard = () => {
           placeholder: '할 일을 적어주세요.',
           defaultValue: todo,
           onChange: onChangeTodo,
+          inputRef: inputRef,
         }}
       />
       <SmallButton
@@ -50,4 +55,4 @@ const CreateCard = () => {
   );
 };
 
-export default CreateCard;
+export default CardCreate;
